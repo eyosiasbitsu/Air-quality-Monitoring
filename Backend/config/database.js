e37ef2b6f2689abc1de5b6cbd5a4599cf530ff34
Fetsum Abyu <eyosiasbitsu@gmail.com>
2024-07-26 00:03:21 +0300

    [Fetsum - backend] Project setup

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
         });
         console.log('MongoDB connected...');
     } catch (err) {
-        console.error('Database connection failed. Exiting now...', err);
+        console.error('Database connection failed. Exiting now...', err.message);
         process.exit(1); // Exit process with failure
     }
 };
