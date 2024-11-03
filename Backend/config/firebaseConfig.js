1f896eaa577897b8eb86c0f6db66c50343b20cad
Fetsum Abyu <eyosiasbitsu@gmail.com>
2024-11-04 00:57:29 +0300

    Added sensor id

--- Backend/config/firebaseConfig.js
+++ Backend/config/firebaseConfig.js
@@ -3,7 +3,7 @@ const serviceAccount = require("../cleanair.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://air-quality-monitor-9777f-default-rtdb.firebaseio.com/sensorData.json?auth=KcTdDDTEihwvKSZXrlViKQheB6QzmCo1R25OPWeR",
+  databaseURL: "",
 });
 
 const firestore = admin.firestore();
