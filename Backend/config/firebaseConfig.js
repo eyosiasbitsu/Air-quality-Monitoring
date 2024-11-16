47713f964d3b1cb24c9419ec859ff6421c2089ba
Fetsum Abyu <eyosiasbitsu@gmail.com>
2024-11-16 16:16:41 +0300

    Configured firebase connection

--- Backend/config/firebaseConfig.js
+++ Backend/config/firebaseConfig.js
@@ -1,12 +1,21 @@
 const admin = require("firebase-admin");
 
-// Load the `cleanair.json` content from the environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+let serviceAccount;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://cleanair-a01ff-default-rtdb.firebaseio.com",
-});
+try {
+  // Load the `cleanair.json` content from the Render secret file
+  serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://cleanair-a01ff-default-rtdb.firebaseio.com",
+  });
+
+  console.log("Firebase initialized successfully.");
+} catch (error) {
+  console.error("Error initializing Firebase:", error.message);
+  process.exit(1); // Exit the process with failure
+}
 
 const db = admin.database();
 
