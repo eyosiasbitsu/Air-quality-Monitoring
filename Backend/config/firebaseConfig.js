fc5165bd86a713c77d3d95abc441896830347ff4
Fetsum Abyu <eyosiasbitsu@gmail.com>
2024-11-17 17:36:58 +0300

    Modified landing page endpoints

--- Backend/config/firebaseConfig.js
+++ Backend/config/firebaseConfig.js
@@ -1,9 +1,9 @@
 const admin = require("firebase-admin");
 const fs = require('fs');
 
-const serviceAccount = require('../cleanair.json');
+// const serviceAccount = require('../cleanair.json');
 
-// const serviceAccount = JSON.parse(fs.readFileSync("/etc/secrets/FIREBASE_CONFIG", "utf8"));
+const serviceAccount = JSON.parse(fs.readFileSync("/etc/secrets/FIREBASE_CONFIG", "utf8"));
 
 if (!admin.apps.length) {
   admin.initializeApp({
