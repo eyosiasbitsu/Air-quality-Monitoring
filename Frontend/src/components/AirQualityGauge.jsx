0d1b3c53ca87dfe14e764bc1e90cc37bdea153f8
leul <leulsegedgebremedhin@gmail.com>
2024-11-19 23:25:09 +0300

    integrate api's with the ui

--- Frontend/src/components/AirQualityGauge.jsx
+++ Frontend/src/components/AirQualityGauge.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactSpeedometer from "react-d3-speedometer";
 
-const AirQualityGauge = () => {
+const AirQualityGauge = ({ pm25 }) => {
+  if (pm25 > 70) pm25 = 70;
   return (
     <div className="bg-black/50 backdrop-blur-md rounded-3xl mt-8 max-w-sm w-full shadow-lg text-white h-fit p-4">
       <h3 className="text-center text-lg font-semibold mb-4">
@@ -9,16 +10,17 @@ const AirQualityGauge = () => {
       </h3>
       <div className="flex items-center justify-center">
         <ReactSpeedometer
-          value={75} // Air Quality value (0-100)
+          value={pm25} // Air Quality value (0-100)
           minValue={0}
-          maxValue={100}
-          segments={5} // Number of segments
+          maxValue={71}
+          segments={4} // Number of segments
+          customSegmentStops={[0, 12, 35.4, 55.4, 71]}
           segmentColors={[
-            "#ff4d4d",
-            "#ff6f6f",
-            "#ffc107",
-            "#8bd23c",
             "#28a745",
+            "#ffc107",
+            "#ff6f6f",
+            "#e20707",
+            // "#8bd23c",
           ]} // Gradient of colors
           needleColor="#ffffff"
           needleBaseColor="#000000" // Dark base circle for needle
@@ -29,14 +31,14 @@ const AirQualityGauge = () => {
           startAngle={-90} // Start angle for semicircle
           endAngle={90} // End angle for semicircle
           customSegmentLabels={[
-            { text: "Critical", position: "OUTSIDE", color: "#fff" },
-            { text: "Danger", position: "OUTSIDE", color: "#fff" },
-            { text: "Moderate", position: "OUTSIDE", color: "#fff" },
-            { text: "", position: "OUTSIDE", color: "#fff" },
             { text: "Healthy", position: "OUTSIDE", color: "#fff" },
+            { text: "Moderate", position: "OUTSIDE", color: "#fff" },
+            { text: "Danger", position: "OUTSIDE", color: "#fff" },
+            { text: "Critical", position: "OUTSIDE", color: "#fff" },
+            // { text: "", position: "OUTSIDE", color: "#fff" },
           ]}
           labelFontSize="8" // Smaller font size for labels
-          currentValueText="" // Hide current value
+          currentValueText="8" // Hide current value
         />
       </div>
       <p className="text-center text-xs mt-4">Last update: Now</p>
